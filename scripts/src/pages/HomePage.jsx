0a2ef0d791ef45caf6d53cfd432ac1b88bb74dc0
JasonBichsel <jason.bichsel.b@gmail.com>
2025-04-08 10:37:38 +0200

    Header welcome section

--- scripts/src/pages/HomePage.jsx
+++ scripts/src/pages/HomePage.jsx
@@ -8,15 +8,12 @@ function HomePage() {
         <div id="homepage-backgrund">
             <nav>
                 <ul>
-                    <li><a href="#header-container">Wilkommen</a></li>
+                    
                 </ul>
             </nav>
             <header>
-                <div id="header-container" className="content-section">
-                    <h1>Hallo, ich bin Jason</h1>
-                    <div id="welcome-section" className="">
-                        <p>Ich bin 17 Jahre alt und bin Auszubildender als Informatiker in der Fachrichtung Applikationsentwicklung.</p>
-                    </div>
+                <div>
+
                 </div>
             </header>
             <main>
